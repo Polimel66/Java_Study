e910e6ab606ab918d4edfdcc8b15bea0fe75adeb
Polimel66 <57943773+Polimel66@users.noreply.github.com>
2022-11-19 18:08:11 +0500

    changes

--- workWithJsoup/src/main/java/Main.java
+++ workWithJsoup/src/main/java/Main.java
@@ -1,4 +1,5 @@
 import org.jsoup.Jsoup;
+
 import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 import java.net.URL;
@@ -6,13 +7,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        var TARGET_URL = "https://faunistics.com/sfinksy/";
-        var USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; rv:106.0) Gecko/20100101 Firefox/106.0";
+        var target_url = "https://faunistics.com/sfinksy/";
+        var user_agent = "Mozilla/5.0 (Windows NT 6.1; rv:106.0) Gecko/20100101 Firefox/106.0";
         var outputPath = "C:\\Users\\79525\\IdeaProjects\\workWithJsoup\\src\\main\\resources";
         Scanner in = new Scanner(System.in);
         try {
-            var document = Jsoup.connect(TARGET_URL)
-                    .userAgent(USER_AGENT)
+            var document = Jsoup.connect(target_url)
+                    .userAgent(user_agent)
                     .referrer("https://www.google.com")
                     .get();
             var elements = document.select("img");
@@ -22,30 +23,25 @@ public class Main {
                 var src = el.absUrl("src");
                 if (src.substring(src.lastIndexOf(".")).equals(format)) {
                     System.out.println("Ссылка на изображение: " + src);
-                    loadImages(src, outputPath);
+                    try {
+                        var indexOfName = src.lastIndexOf("/");
+                        var name = src.substring(indexOfName);
+                        System.out.printf("Имя файла: %s\n", name);
+                        var url = new URL(src);
+                        var input = url.openStream();
+                        var output = new BufferedOutputStream(new FileOutputStream(outputPath + name));
+                        for (int i; (i = input.read()) != -1; ) {
+                            output.write(i);
+                        }
+                        output.close();
+                        input.close();
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
                 }
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-    }
-
-    private static void loadImages(String src, String outputPath) {
-        try {
-            var indexOfName = src.lastIndexOf("/");
-            var name = src.substring(indexOfName);
-            System.out.printf("Имя файла: %s\n", name);
-            var url = new URL(src);
-            var in = url.openStream();
-            var out = new BufferedOutputStream(new FileOutputStream(outputPath + name));
-            for (int i; (i = in.read()) != -1; ) {
-                out.write(i);
-            }
-            out.close();
-            in.close();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-
     }
 }
